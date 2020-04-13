de152cb58b17e45444256a4c3ed80fe29a36c78c
Lei Liu <liulei18119@gmail.com>
2020-04-13 11:19:45 -0700

    Update the url for the repository in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python module manupulating VASP files",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/LeiLiu8102/vasplib_py.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
